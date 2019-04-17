edb28200addfdad7075c0b2b0500987d6a5f8615
igor gawrys <prezes@socialler.pl>
2019-04-17 22:04:13 +0200

    Basic writed

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index' );
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource( 'apps', 'AppsController' );
+
+Route::resource( 'docs', 'DocsController' );
